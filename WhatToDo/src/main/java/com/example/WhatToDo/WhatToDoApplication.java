1c907023610de623b5a91baec485f015d41e74b5
kristupas <kristupas.jovaisa03@gmail.com>
2021-12-20 16:20:09 +0200

    First commit

---
+++ WhatToDo/src/main/java/com/example/WhatToDo/WhatToDoApplication.java
@@ -0,0 +1,13 @@
+package com.example.WhatToDo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatToDoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatToDoApplication.class, args);
+	}
+
+}
